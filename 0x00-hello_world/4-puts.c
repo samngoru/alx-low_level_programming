83fab18b799759c56be443f3ef2083f78bf09f31
samngoru <samngoru@gmail.com>
2023-03-16 13:21:02 -0700

    puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - program printing a line with puts function
+ * return: 0 (eureka!)
+ */
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
