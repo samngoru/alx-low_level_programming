8bc06166161bf31605aecd17ecbc1dfb5b4a6b19
samngoru <samngoru@gmail.com>
2023-03-21 18:00:38 -0700

    98 battery street the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,7 +15,9 @@ void print_to_98(int n)
 	{
 		for (i = n ; i <= 98 ; i++)
 		{
-			printf("%d, ", i);
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
 		}
 		printf("\n");
 	}
@@ -23,7 +25,9 @@ void print_to_98(int n)
 	{
 		for (i = n ; i <= 98 ; i++)
 		{
-			printf("%d, ", i);
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
 		}
 		printf("\n");
 	}
@@ -32,9 +36,11 @@ void print_to_98(int n)
 		for (i = n ; i >= 98 ; i--)
 		{
 			printf("%d, ", i);
+			if (i != 98)
+				printf(", ");
 		}
 		printf("\n");
 	}
 	else if (n == 98)
-		printf("%d,", n);
+		printf("%d", n);
 }
