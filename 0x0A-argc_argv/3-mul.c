661c2bff805236c53b9ca3afd080d2f407b15129
samngoru <samngoru@gmail.com>
2023-04-10 19:57:40 -0700

    multiplis two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,7 +9,6 @@
  */
 
 int _atoi(char *s)
-
 {
 	int i, d, n, len, f, digit;
 
@@ -21,46 +20,29 @@ int _atoi(char *s)
 	digit = 0;
 
 	while (s[len] != '\0')
-		{
 		len++;
-		}
 	while (i < len && f == 0)
 	{
 		if (s[i] == '-')
-		{
 			++d;
-		}
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = s[i] - '0';
 			if (d % 2)
-			{
+
 				digit = -digit;
-				n = n * 10 + digit;
-				f = 1;
-			}
+			n = n * 10 + digit;
+			f = 1;
 			if (s[i + 1] < '0' || s[i + 1] > '9')
-			{
 				break;
 				f = 0;
-			}
 		}
 		i++;
 	}
 	if (f == 0)
-	{
 		return (0);
-	}
 	return (n);
 }
-
-/**
- * main - multiplies two numbers
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: 0 (Success), 1 (Error)
- */
 
 int main(int argc, char *argv[])
 
@@ -72,7 +54,6 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-
 	num1 = _atoi(argv[1]);
 	num2 = _atoi(argv[2]);
 	result = num1 * num2;
